b0f546b11ba661808fca897f635dc1c848cc46da
Tobias <tobiasbraje@gmail.com>
2024-05-28 07:53:34 +0100

    updates

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,10 @@
+require('telescope').setup{ 
+  defaults = { 
+    file_ignore_patterns = { 
+      "node_modules" 
+    }
+  }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- lua/tobyvim/packer.lua
+++ lua/tobyvim/packer.lua
@@ -43,7 +43,7 @@ return require('packer').startup(function(use)
 }
 use {'mfussenegger/nvim-dap'}
 use {'mfussenegger/nvim-dap-python'}
-use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
 use { "averms/black-nvim", run = ":UpdateRemotePlugins"}
 use {
   'nvim-lualine/lualine.nvim',
